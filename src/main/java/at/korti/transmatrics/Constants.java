b061c7cf25b0997e6997804139e2cec550cbee83
Korti <lucas.korthauer@gmail.com>
2016-02-24 22:02:12 +0100

    Added the Constants class for all constants.
    Configured the gradle build script.
    Configured the circle.yml to build after the gradlew dependencies command.
    Added the build status to the README.md.

---
+++ src/main/java/at/korti/transmatrics/Constants.java
@@ -0,0 +1,12 @@
+package at.korti.transmatrics;
+
+/**
+ * Created by Korti on 24.02.2016.
+ */
+public final class Constants {
+    public static class Mod{
+        public static final String MODID = "Transmatrics";
+        public static final String NAME = "Transmatrics";
+        public static final String VERSION = "@VERSION@";
+    }
+}
